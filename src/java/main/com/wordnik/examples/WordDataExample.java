0bff9e5d90eb1061a0afc5829ad871dd9bc75da1
Tony Tam <tony@wordnik.com>
2010-03-08 10:48:30 -0800

    added new Dictionary Model method examples

--- src/java/main/com/wordnik/examples/WordDataExample.java
+++ src/java/main/com/wordnik/examples/WordDataExample.java
@@ -5,10 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.sun.jersey.api.client.GenericType;
-import com.wordnik.examples.objects.Example;
-import com.wordnik.examples.objects.Frequency;
-import com.wordnik.examples.objects.FrequencyValue;
-import com.wordnik.examples.objects.Word;
+import com.wordnik.examples.objects.*;
 
 /**
  * fetches a word and some properties on it
@@ -25,13 +22,18 @@ public class WordDataExample extends AbstractExample {
 
 		//	your api key
 		API_KEY = args[0];
+		
+		//	word to get data for
+		String wordstring = args[1];
 	
 		WordDataExample t = new WordDataExample();
 		try{
 			t.initClient();
-			t.fetchWord(args[1]);
-			t.fetchExamples(args[1]);
-			t.fetchFrequency(args[1]);
+			t.fetchWord(wordstring);
+			t.fetchExamples(wordstring);
+			t.fetchFrequency(wordstring);
+			t.fetchDefinitions(wordstring);
+			t.fetchRelatedWords(wordstring);
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -46,9 +48,44 @@ public class WordDataExample extends AbstractExample {
 	 */
 	void fetchWord(String wordstring) throws Exception {
 		Word word = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8")).header("api_key", API_KEY).get(Word.class);
-		System.out.println("found word " + word.getWord());
+		System.out.println("found word " + word.getWordstring());
+	}
+
+	/**
+	 * fetches definitions for the specified word
+	 * 
+	 * @param wordstring
+	 * @throws Exception
+	 */
+	void fetchDefinitions(String wordstring) throws Exception {
+		GenericType<List<Definition>> genericType = new GenericType<List<Definition>>() {}; 
+		List<Definition> definitions = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8") + "/definitions").header("api_key", API_KEY).get(genericType);
+		
+		for(Definition definition : definitions){
+			System.out.println("definition: " + definition.getText());
+		}
+	}
+
+	/**
+	 * fetches frequency data for the specified word
+	 * @param wordstring
+	 * @throws Exception
+	 */
+	void fetchFrequency(String wordstring) throws Exception {
+		FrequencySummary frequencyData = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8") + "/frequency").header("api_key", API_KEY).get(FrequencySummary.class);
+		
+		for(Iterator<FrequencyValue> x =frequencyData.getFrequency().iterator(); x.hasNext();){
+			FrequencyValue frequency = x.next();
+			System.out.println("Year: " + frequency.getYear() + ", frequency: " + frequency.getCount());
+		}
 	}
 	
+	/**
+	 * fetches examples for the specified word
+	 * 
+	 * @param wordstring
+	 * @throws Exception
+	 */
 	void fetchExamples(String wordstring) throws Exception {
 		GenericType<List<Example>> genericType = new GenericType<List<Example>>() {}; 
 		List<Example> examples = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8") + "/examples").header("api_key", API_KEY).get(genericType);
@@ -57,14 +94,22 @@ public class WordDataExample extends AbstractExample {
 			System.out.println("example: " + example.getDisplay());
 		}
 	}
-
-	void fetchFrequency(String wordstring) throws Exception {
-		Frequency frequencyData = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8") + "/frequency").header("api_key", API_KEY).get(Frequency.class);
+	
+	/**
+	 * fetches related words for the specified word
+	 * 
+	 * @param wordstring
+	 * @throws Exception
+	 */
+	void fetchRelatedWords(String wordstring) throws Exception {
+		GenericType<List<Related>> genericType = new GenericType<List<Related>>() {}; 
+		List<Related> relatedWords = client.resource("http://api.wordnik.com/api/word.xml/" + URLEncoder.encode(wordstring, "utf8") + "/related").header("api_key", API_KEY).get(genericType);
 		
-		
-		for(Iterator<FrequencyValue> x =frequencyData.getValues().iterator(); x.hasNext();){
-			FrequencyValue frequency = x.next();
-			System.out.println("Year: " + frequency.getYear() + ", frequency: " + frequency.getCount());
+		for(Related relatedWord : relatedWords){
+			System.out.println("relationship: " + relatedWord.getRelType());
+			for(String word : relatedWord.getWordstrings().getWordstring()){
+				System.out.println("\t"+word);
+			}
 		}
 	}
 	
