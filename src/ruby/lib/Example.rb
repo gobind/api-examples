5c47bbfa74559ad0e9a3489c617dac76f0d1ab24
altay <aguvench@gmail.com>
2010-05-26 16:37:03 -0700

    updated ruby wrapper to include List CRUD examples

--- src/ruby/lib/Example.rb
+++ src/ruby/lib/Example.rb
@@ -3,11 +3,11 @@ class Example
   attr_accessor :display, :rating, :url, :title, :year
   
   def initialize(options={})
-    @display = options[:display]
-    @rating = options[:rating]
-    @url = options[:url]
-    @title = options[:title]
-    @year = options[:year]
+    @display = options['display']
+    @rating = options['rating']
+    @url = options['url']
+    @title = options['title']
+    @year = options['year']
   end
   
 end
--- src/ruby/lib/wordnik.rb
+++ src/ruby/lib/wordnik.rb
@@ -1,57 +1,76 @@
+require 'rubygems'
 require 'httparty'
+require 'json'
 require 'word'
 require 'definition'
 require 'example'
+require 'list'
 
 # Ruby wrapper to access Wordnik definitions API. Has dependency on HTTParty gem.
 # No exception handling or other niceties. Intended as an API demo, not meant for production use
 class Wordnik
   include HTTParty
   base_uri 'http://api.wordnik.com/api'
-  headers 'Content-Type' => 'application/json'
   
-  API_KEY = 'your_api_key_here'
+  attr_accessor :api_key, :auth_token
 
-  # get an array of Example objects for a word
-  def self.examples(word)
-    raw_examples = self.get("/word.json/#{word}/examples", {:headers => {'api_key' => API_KEY}} )
-    
-    examples = Array.new
-    raw_examples.each do |example|
-      examples << Example.new(  :display => example['display'],
-                                :rating => example['rating'],
-                                :url => example['url'],
-                                :title => example['title'] )
+  def authenticated?
+    return !self.auth_token.blank?
+  end
+
+  def ensure_authentic
+    raise("This method requires a valid auth_token!") unless self.authenticated?
+  end
+
+  @@client = nil
+  def self.client
+    return @@client
+  end
+
+  # this initializes the wordnik api client
+  # options[:api_key] is REQUIRED 
+  # if you want to access the "write" api calls (e.g. List creation/deletion), you need to also pass in options[:username] and options[:password] to authenticate
+  # e.g. w = Wordnik.new({:username=>'my_username', :password=>'my_password', :api_key=>'my_api_key'})
+  # this will set the @api_key, @auth_token, and @user_id variables
+  def initialize(options={}) 
+    raise("ERROR: missing api_key!") if options[:api_key].blank? 
+    @api_key = options[:api_key]
+    # pass in options[:username] and options[:password] if you want to authenticate this instance of the api client.
+    # authenticating will allow you to perform all CRUD operations (e.g. List creation/deletion/etc)
+    if options[:username] && options[:password]
+      response = self.class.get("/account.json/authenticate/#{options[:username]}", {:query=>{:password=>options[:password]}, :headers => {'Content-Type'=>'application/json', 'api_key' => @api_key}} )
+      if (response['type'] && response['type']=='error')
+        puts("ERROR: #{response['message']}")
+      elsif (response['userId'])
+        puts('access GRANTED!')
+        @auth_token = response['token']
+        @user_id = response['userId']
+      else
+        puts("access DENIED! make sure you're passing a valid :username, :password, and :api_key to Wordnik.new")
+      end
     end
-    examples
-  end
-
-  # get an array of Definition objects for a word
-  def self.definitions(word)
-      raw_defs = self.get("/word.json/#{word}/definitions", {:headers => {'api_key' => API_KEY}} )
-      
-      definitions = Array.new
-      raw_defs.each do |definition|
-        definitions << Definition.new( :word => definition['headword'],
-                                       :text => definition['text'],
-                                       :extended_text => definition['extendedText'],
-                                       :part_of_speech => definition['partOfSpeech'] )
-      end
-      definitions
+    @@client = self # so we can do Wordnik.client
+    return self
   end
   
-  # given a wordstring, get an array of related words
-  def self.related(word)
-      raw_related = self.get("/word.json/#{word}/related", {:headers => {'api_key' => API_KEY}} )
-      
-      words = Array.new
-      raw_related.each do |type|
-        type['wordstrings'].each do |word|
-          words << Word.new( :wordstring => word,
-                             :rel_type => type['relType'] )
-        end
-      end
-      
-      words
+  # this returns the api headers (including api_key and auth_token) for the wordnik api client
+  def api_headers
+    the_headers = {'Content-Type'=>'application/json', 'api_key' => @api_key}
+    the_headers.merge!({'auth_token'=>@auth_token}) if authenticated?
+    return the_headers
   end
+
+  # get all the lists for the authenticated user
+  def lists
+    ensure_authentic
+    list_data = Wordnik.get("/wordLists.json", {:headers=>self.api_headers})
+    return list_data.map{|list| List.new(list) }
+  end
+
+  # create a list for the authenticated user
+  def create_list(name, description)
+    ensure_authentic
+    return List.create(name, description)
+  end
+
 end
